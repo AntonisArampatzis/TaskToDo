bfeb311ba0f82b14e7d9295f1b987d5b4ed5fd44
AntonisArampatzis <arabatzisantonis@yahoo.com>
2025-07-31 12:38:26 +0300

    Moved secret key and db to .env

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,20 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 migrate = Migrate() 
 
 app = Flask(__name__)
 
+load_dotenv()
 # Config
-dbName = 'TaskToDoDB'
-secret_key = 'PrdzVUXphvAlDtKZfgy'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:353535@localhost/{dbName}'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:353535@localhost/{dbName}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize db and migrate and set CORS
